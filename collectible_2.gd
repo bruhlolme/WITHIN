521b72f7e13cd034438f43e20ac1ae9c83a55fc5
Dana-WITHIN <dana.alawawdeh@stu.ocsb.ca>
2025-06-19 07:12:30 -0400

    Revert "Add files via upload"

--- collectible_2.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-signal collected
-
-func _on_body_entered(body):
-	if body.name == "Player":
-		emit_signal("collected")
-		queue_free()
--- door.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-var is_unlocked = false
-var door_opened = false
-var ready_for_transition = false
-
-func unlock():
-	is_unlocked = true
-	$AnimatedSprite2D.play("DoorOpen")
